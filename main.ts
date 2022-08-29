feaca35e26f53fd35cf995ef24d2e21369c149b1
Josh Bright <josh.bright@gmail.com>
2022-08-29 16:37:09 -0700

    We now process the url short codes within the document, but, only when the area using those tags changes, which I can't figure out how to override.  WIP

--- main.ts
+++ main.ts
@@ -1,6 +1,31 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { 
+	App, 
+	Editor, 
+	MarkdownView, 
+	Modal, 
+	Notice, 
+	Plugin, 
+	PluginSettingTab, 
+	Setting, 
+	MarkdownRenderChild, 
+	MarkdownRenderer 
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+
+class ShortUrlConverter extends MarkdownRenderChild {
+	text: string;
+	constructor(containerEl: HTMLElement, text: string) {
+		super(containerEl);
+		this.text = text;
+	}
+
+	onload() {
+		const urlName = this.text.replaceAll("+", "");
+		const el = `<a href="#">${urlName}</a>`
+		this.containerEl.innerHTML = el;
+	}
+}
+
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -15,8 +40,9 @@ function getFrontMatter() {
 
 	const file = app.workspace.getActiveFile();
 	let fmc = app.metadataCache.getFileCache(file)?.frontmatter;
-	
+
 	for (let key in fmc) {
+		console.log(key)
 		if (key == "position") continue;
 
 		result[key] = fmc[key];
@@ -27,16 +53,17 @@ function getFrontMatter() {
 
 export default class UrlShortenerPlugin extends Plugin {
 	settings: MyPluginSettings;
+	currentMetadata = {};
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			new Notice("Happy Friday!");
+			// new Notice("Happy Friday!");
 
 			let data = getFrontMatter();
-			console.log(data)
+			
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -45,54 +72,32 @@ export default class UrlShortenerPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.currentMetadata = getFrontMatter()
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.registerMarkdownPostProcessor((element, context) => {
+			console.log("Rendering Markdown")
+			let data = getFrontMatter();
+			const cells = element.querySelectorAll("td");
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+      		for (let index = 0; index < cells.length; index++) {
+        		const cell = cells.item(index);
+        		
+        		if (cell.innerText.startsWith("+") && cell.innerText.endsWith("+")) {
+        			let urlName = cell.innerText.replaceAll("+", "");
+        			let fixedUrlName = urlName.replace("url.", "");
+        			const url = data[urlName];
+        			cell.innerHTML = `<a href='${url}'>${fixedUrlName}</a>`
+        		}
+      		}
+    	});
+
+    	this.registerDomEvent(document, 'keyup', (evt) => {
+			console.log("Keyup!")
+
+		});
 	}
 
 	onunload() {
@@ -152,3 +157,6 @@ class SampleSettingTab extends PluginSettingTab {
 				}));
 	}
 }
+
+
+
